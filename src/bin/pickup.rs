89babaff033ba672a02ffa64d762a3f164672420
Aaron Burdick <burdick.aaron@gmail.com>
2021-04-25 16:15:32 +0900

    feat: add logging to bin

--- src/bin/pickup.rs
+++ src/bin/pickup.rs
@@ -2,8 +2,11 @@ use std::io;
 use pickup::{Pickup, PickupOpts};
 use pickup::reader::{Reader};
 use pickup::printer::{Printer};
+use env_logger;
 
 fn main() -> io::Result<()> {
+    env_logger::init();
+    
     let stdio = io::stdin();
     let stdin = stdio.lock();
     let stdout = io::stdout();
